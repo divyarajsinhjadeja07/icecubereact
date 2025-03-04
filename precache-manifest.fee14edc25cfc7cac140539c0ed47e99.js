dc75519db9258883f3d349642e687c7c4b31d78a
divyarajsinhjadeja07 <155974577+divyarajsinhjadeja07@users.noreply.github.com>
2025-03-04 16:52:20 +0530

    Updates

---
+++ precache-manifest.fee14edc25cfc7cac140539c0ed47e99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca217597b7d850f731a61f04da85334",
+    "url": "/icecubereact/index.html"
+  },
+  {
+    "revision": "f0758cc07516cd1d9920",
+    "url": "/icecubereact/static/css/main.fdc3092f.chunk.css"
+  },
+  {
+    "revision": "8898ca240972eb1a5713",
+    "url": "/icecubereact/static/js/2.1939633e.chunk.js"
+  },
+  {
+    "revision": "1aef9e0d481a6908c30509eb88500877",
+    "url": "/icecubereact/static/js/2.1939633e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5759bc8fa52f998f6db8",
+    "url": "/icecubereact/static/js/3.547a162e.chunk.js"
+  },
+  {
+    "revision": "f0758cc07516cd1d9920",
+    "url": "/icecubereact/static/js/main.960ca805.chunk.js"
+  },
+  {
+    "revision": "0acfcfd5c5291f97e104",
+    "url": "/icecubereact/static/js/runtime-main.e4e8af06.js"
+  }
+]);
