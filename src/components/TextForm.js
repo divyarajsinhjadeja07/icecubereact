da1f4d388390101bc5eb389352216310de846a81
divyarajsinhjadeja07 <155974577+divyarajsinhjadeja07@users.noreply.github.com>
2025-03-05 15:12:40 +0530

    re-deploy

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -51,15 +51,15 @@ export default function TextForm(props) {
           <div className="mb-3">
               <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
           </div>
-          <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-          <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-          <button className="btn btn-primary mx-2" onClick={handleCopyClick}>Copy to Clipboard</button>
-          <button className="btn btn-primary mx-2" onClick={handleDownload}>Download Text</button>
+          <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+          <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+          <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleCopyClick}>Copy to Clipboard</button>
+          <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleDownload}>Download Text</button>
       </div>
       <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
         <h2>Your text summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
-        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:'Enter something in the textbox above to preview it...'}</p>
       </div>
